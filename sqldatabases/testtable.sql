8f524c0575ae229ec3a4bb4aa1b99cab98dee740
JamesT3 <eeu4b0@bangor.ac.uk>
2017-04-07 21:26:30 +0100

    edited testtable.sql Added course column

--- sqldatabases/testtable.sql
+++ sqldatabases/testtable.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
--- https://www.phpmyadmin.net/
+-- version 4.5.1
+-- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 07, 2017 at 03:02 PM
--- Server version: 10.1.21-MariaDB
--- PHP Version: 5.6.30
+-- Generation Time: Apr 07, 2017 at 10:11 PM
+-- Server version: 10.1.19-MariaDB
+-- PHP Version: 5.6.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -29,19 +29,20 @@ SET time_zone = "+00:00";
 CREATE TABLE `testtable` (
   `FName` varchar(100) NOT NULL,
   `LName` varchar(100) NOT NULL,
-  `ID` int(8) NOT NULL
+  `ID` int(8) NOT NULL,
+  `course` char(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `testtable`
 --
 
-INSERT INTO `testtable` (`FName`, `LName`, `ID`) VALUES
-('Jacob', 'Teale', 1),
-('George', 'Steele', 2),
-('James', 'Trollope', 3),
-('Jean Paul', 'Joseph', 4),
-('Dom', 'Radford', 5);
+INSERT INTO `testtable` (`FName`, `LName`, `ID`, `course`) VALUES
+('Jacob', 'Teale', 1, 'CIS'),
+('George', 'Steele', 2, 'CIS'),
+('James', 'Trollope', 3, 'CS'),
+('Jean Paul', 'Joseph', 4, 'CIS'),
+('Dom', 'Radford', 5, 'CIS');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
